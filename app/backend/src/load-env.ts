e186ca04fa66ac58da116c16135fec7253d78801
IvanMakovetskyi <im061056@gmail.com>
2026-05-04 10:03:25 -0700

    payment webhook fix

--- app/backend/src/load-env.ts
+++ app/backend/src/load-env.ts
@@ -2,10 +2,15 @@ import path from "path";
 import dotenv from "dotenv";
 
 dotenv.config({
-  path: path.resolve(process.cwd(), "apps/backend/.env"),
+  path: path.resolve(process.cwd(), "app/backend/.env"),
 });
 
 dotenv.config({
   path: path.resolve(process.cwd(), ".env"),
+  override: false,
+});
+
+dotenv.config({
+  path: path.resolve(process.cwd(), "apps/backend/.env"),
   override: false,
 });
