447bc1228645db332299dc0a5f25fe80bd11c5b2
Ivan Makovetskyi <im061056@gmail.com>
2026-05-22 14:56:06 -0700

    pixel addition

--- app/web/src/app/(public)/layout.tsx
+++ app/web/src/app/(public)/layout.tsx
@@ -1,4 +1,6 @@
+import Script from "next/script";
 import { cookies } from "next/headers";
+
 import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
 import { CookieConsentBanner } from "@/components/layout/CookieConsentBanner";
@@ -10,16 +12,57 @@ export default async function PublicLayout({
   children: React.ReactNode;
 }) {
   const cookieStore = await cookies();
+
   const localeCookie = cookieStore.get("ibpa-locale")?.value;
+
   const initialLocale: Locale =
-    localeCookie === "ru" || localeCookie === "uk" ? localeCookie : "en";
+    localeCookie === "ru" || localeCookie === "uk"
+      ? localeCookie
+      : "en";
+
   const dashboardHref = "/dashboard";
 
   return (
     <I18nProvider initialLocale={initialLocale}>
+      {/* Meta Domain Verification */}
+      <meta
+        name="facebook-domain-verification"
+        content="1oij9v4q95qd0cudvfkr5tnmmgr2yw"
+      />
+
+      {/* Meta Pixel */}
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+
+          fbq('init', '1312423774195049');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=1312423774195049&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
+
       <Navbar dashboardHref={dashboardHref} />
+
       <main>{children}</main>
+
       <Footer />
+
       <CookieConsentBanner />
     </I18nProvider>
   );
